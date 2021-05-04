00025dc4f478a8ded7dadcc42971ea9ab3b37b95
Dumazeau <hdumazeau@richcongress.com>
2021-05-04 18:17:20 +0200

    Add excel

--- Builder/SheetRowContentBuilder.php
+++ Builder/SheetRowContentBuilder.php
@@ -3,16 +3,12 @@
 namespace RichId\ExcelGeneratorBundle\Builder;
 
 use RichId\ExcelGeneratorBundle\Config\ExcelSheetGeneratorConfiguration;
-use RichId\ExcelGeneratorBundle\Data\Annotation\BorderStyle;
 use RichId\ExcelGeneratorBundle\Data\Annotation\ColumnMerge;
 use RichId\ExcelGeneratorBundle\Data\Annotation\ContentStyle;
-use RichId\ExcelGeneratorBundle\Data\Annotation\GroupBorderStyle;
 use RichId\ExcelGeneratorBundle\Data\Export;
-use RichId\ExcelGeneratorBundle\Data\ExportWithChildren;
 use RichId\ExcelGeneratorBundle\Utility\AnnotationReaderTrait;
 use RichId\ExcelGeneratorBundle\Utility\CellStyleUtility;
 use RichId\ExcelGeneratorBundle\Utility\PropertyUtility;
-use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -47,7 +43,6 @@ final class SheetRowContentBuilder
     {
         $currentRow = $sheet->getHighestRow() === 1 ? 2 : $sheet->getHighestRow() + 1;
         $properties = $this->propertyUtility->getPropertiesForConfigAndData($configuration, $rowContent);
-//        $from = \sprintf('%s%d', Coordinate::stringFromColumnIndex(1), $currentRow);
 
         foreach ($properties as $key => $property) {
             $contentStyle = $this->getPropertyAnnotationForProperty($property->getReflectionProperty(), ContentStyle::class);
@@ -63,21 +58,5 @@ final class SheetRowContentBuilder
                 $this->cellStyleUtility->setStyleOfDataFor($sheet, $contentStyle, $currentRow, $key + 1, $rowContent);
             }
         }
-
-//        $to = \sprintf('%s%d', Coordinate::stringFromColumnIndex($key ? $key + 1 : 0), $sheet->getHighestRow());
-//
-//        $groupBorderStyle = $this->getClassAnnotationFor(\get_class($rowContent), GroupBorderStyle::class);
-//        if ($groupBorderStyle !== null) {
-//            $styleArray = [
-//                'borders' => [
-//                    'outline' => [
-//                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_HAIR,
-//                        'color' => ['rgb' => '000000'],
-//                    ],
-//                ],
-//            ];
-//
-//            $sheet->getStyle(sprintf('%s:%s', $from, $to))->applyFromArray($styleArray);
-//        }
     }
 }
